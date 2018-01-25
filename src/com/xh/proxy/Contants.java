85b18966db624bf7d4d9c59e4770889b99d0c4d9
liuhuiiang <825378291@qq.com>
2018-01-25 15:18:36 +0800

    Create Contants.java

---
+++ src/com/xh/proxy/Contants.java
@@ -0,0 +1,10 @@
+package com.xh.proxy;
+
+/**
+ * @version 创建时间：2018-1-12 上午10:50:57 项目：proxy 包名：com.xh.proxy 文件名：Contants.java
+ *          作者：lhl 说明:
+ */
+
+public class Contants {
+	public final static String CLASS_NAME = "class_name";
+}
